49c1aca63befc32d83a7ff1031c8d315033ed341
Stanislaw <staskargapolcev26@gmail.com>
2025-05-18 22:08:57 +0300

    enemiesInitial

--- src/assets/enemiesInitial.ts
+++ src/assets/enemiesInitial.ts
@@ -3,8 +3,7 @@ import { IArmoredEnemy, IEnemy } from "../types/enemies.type";
 export const OrdinaryEnemyInitial: IEnemy = {
   vector: 1,
   index: null,
-  flashing:
-    this.index == 4 || this.index == 11 || this.index == 18 ? true : false,
+  flashing: false,
   moveInterval: null,
   shotInterval: null,
   bullet: null,
@@ -15,8 +14,7 @@ export const OrdinaryEnemyInitial: IEnemy = {
 export const FastEnemyInitial: IEnemy = {
   vector: 1,
   index: null,
-  flashing:
-    this.index == 4 || this.index == 11 || this.index == 18 ? true : false,
+  flashing: false,
   moveInterval: null,
   shotInterval: null,
   bullet: null,
@@ -27,8 +25,7 @@ export const FastEnemyInitial: IEnemy = {
 export const RapidFireEnemyInitial: IEnemy = {
   vector: 1,
   index: null,
-  flashing:
-    this.index == 4 || this.index == 11 || this.index == 18 ? true : false,
+  flashing: false,
   moveInterval: null,
   shotInterval: null,
   bullet: null,
@@ -39,8 +36,7 @@ export const RapidFireEnemyInitial: IEnemy = {
 export const ArmoredEnemyInitial: IArmoredEnemy = {
   vector: 1,
   index: null,
-  flashing:
-    this.index == 4 || this.index == 11 || this.index == 18 ? true : false,
+  flashing: false,
   moveInterval: null,
   shotInterval: null,
   bullet: null,
